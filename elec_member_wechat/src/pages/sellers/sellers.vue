6907ffd390179c64d9fbfe26e7f64f225424be65
xvlei <821836023@qq.com>
2017-11-01 17:27:17 +0800

    ss

--- elec_member_wechat/src/pages/sellers/sellers.vue
+++ elec_member_wechat/src/pages/sellers/sellers.vue
@@ -79,8 +79,7 @@
                                              <img :src="v.logo1" alt="" style="width:1.1rem;height:0.85rem">
                                              <div class="imgright">
                                                  <h1 style="padding-bottom:0.1rem">{{v.shop_name}}</h1>
-                                                 
-                                                 <h3 style="padding-top:0.1rem">{{v.industry_name}}</h3>
+                                                 <h3 style="padding-top:-0.2rem">{{v.industry_name}}</h3>
                                                  <img class="imgleft" src="static/img/jf.png" alt="">
                                                  <h2 style="padding-top:-0.4rem">{{v.berth_number}}</h2>
                                              </div>
