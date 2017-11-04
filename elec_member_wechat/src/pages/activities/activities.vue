6a75286cc72a6fd02f36ebb7611d0677126e963d
xvlei <821836023@qq.com>
2017-11-04 14:49:35 +0800

    sdasdasdaszxc

--- elec_member_wechat/src/pages/activities/activities.vue
+++ elec_member_wechat/src/pages/activities/activities.vue
@@ -1,29 +1,36 @@
 <template>
     <!--最新活动-->
     <div style="background-color:#FFFFFF">
-           <ul>
+           <ul style="padding-bottom:0.2rem">
                   <li>
+                    <router-link to="/activeDetail">
                         <figure>
-                          <router-link to="/activeDetail">
+                          
                                <img src="static/img/activeTest.jpg" alt="">
                                <figcaption>花艺体验活动来啦,慢下来,给自己一朵花开的时间给自己一朵花开的时间</figcaption>
-                          </router-link>
+                          
                         </figure>
                         <div>
                           <label style="height:0.22rem;color:#999">2017-09-14~2017-09-15</label>
                           <button disabled="disabled" style="border: 1px solid #CACACA;width:0.6rem">进行中</button>
                         </div>
+                        </router-link>
                   </li>
                    <li>
+                     <router-link to="/activeDetail">
                      <figure>
+                       
                        <img src="static/img/activeTest.jpg" alt="">
                        <figcaption>花艺体验活动来啦,慢下来,给自己一朵花开的时间给自己一朵花开的时间</figcaption>
+                       
                      </figure>
                      <div>
                        <label style="height:0.22rem;color:#999">2017-09-14~2017-09-15</label>
                        <button disabled="disabled" style="border: 1px solid #CACACA;width:0.6rem">进行中</button>
                      </div>
+                     </router-link>
                    </li>
+                    
            </ul>
     </div>
 </template>
