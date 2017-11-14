b932f92b0c68ca07c7a1943de4de6fa56e977bff
lumin824 <lumin824@163.com>
2017-11-14 14:34:04 +0800

    no message

--- elec_member_wechat/src/pages/ticketsUpload/ticketsUpload.vue
+++ elec_member_wechat/src/pages/ticketsUpload/ticketsUpload.vue
@@ -50,8 +50,8 @@
     </ul>
 
     <div style="display:flex;font-size:0.9em;color:#666;margin: 0 10px;flex:1;">
-      <div style="white-space:nowrap;width:4.5em;">温馨提示：</div>
-       <ul style="text-align:left;text-indent:-0.9em;margin-left:0.9em;">
+      <div style="white-space:nowrap;background-color:red;">温馨提示：</div>
+       <ul style="text-align:left;text-indent:-0.9em;margin-left:0.9em;background-color:green;">
          <li>
            1 小票有效期有三个月，请及时上传
          </li>
