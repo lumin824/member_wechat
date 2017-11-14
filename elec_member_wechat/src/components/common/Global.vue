c74a545416a2b18a144658dec0f507bd6e5d0db6
lumin824 <lumin824@163.com>
2017-11-14 15:23:27 +0800

    no message

--- elec_member_wechat/src/components/common/Global.vue
+++ elec_member_wechat/src/components/common/Global.vue
@@ -1,10 +1,12 @@
 <script type="text/javascript">
   //全局静态变量
   const mallId = 1;
+  const apiHost = 'http://121.196.208.176:9001';
   const appId = 'wx3041b222eaad5c8a';
   export default
   {
     mallId,
+    apiHost,
     appId,
   }
 </script>
