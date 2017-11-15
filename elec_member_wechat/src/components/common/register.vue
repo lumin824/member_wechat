96de41eb88783d775e873cea9d944e2e4370ad7c
xvlei <821836023@qq.com>
2017-11-15 20:02:32 +0800

    1

--- elec_member_wechat/src/components/common/register.vue
+++ elec_member_wechat/src/components/common/register.vue
@@ -51,6 +51,9 @@
             if(this.formData.code==''){
                   this.formData.v_code= '请先获取验证码';
                   this.c_state = 'error';
+            }else if(this.formData.code.length<6){
+                   this.formData.v_code= '请输入正确的验证码';
+                  this.c_state = 'error';
             }else{
                 //  手机号验证码都验证通过,验证验证码是否正确
                 this.$http.post('http://121.196.208.176:9001/member/vcodeCheck',{'mobile':this.formData.phone,'vcode':this.formData.code }).then(data =>{
